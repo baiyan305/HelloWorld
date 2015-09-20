7298b77a65ba123ad8398cefe04e1ba9a231f808
YanBai <baiyan718@gmail.com>
2015-09-20 13:16:46 -0500

    Initial commit

---
+++ src/com/company/instance/LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,20 @@
+package com.company.instance;
+
+import com.company.support.Test;
+import com.company.support.TreeNode;
+
+/**
+ * Created by Yan on 9/19/15.
+ */
+public class LowestCommonAncestorOfBinaryTree implements Test{
+
+    @Override
+    public void execute() {
+
+    }
+
+    private TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+
+    }
+
+}
